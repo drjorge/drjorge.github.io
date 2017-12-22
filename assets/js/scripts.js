acec10774f309286fbbdb4dc020deec6e2b6dc74
Marysol Sanchez <marysol@MacBook-Air-de-Marysol-Sanchez.local>
2017-12-22 13:00:45 -0600

    Initial changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -122,7 +122,7 @@ jQuery(function ($) {
     // -------------------------------------------------------------
     // Progress Bar
     // -------------------------------------------------------------
- 
+
     $('.skill-progress').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
         if (visible) {
             $.each($('div.progress-bar'),function(){
@@ -131,7 +131,7 @@ jQuery(function ($) {
             $(this).unbind('inview');
         }
     });
-    
+
     // -------------------------------------------------------------
     // More skill
     // -------------------------------------------------------------
@@ -235,9 +235,9 @@ jQuery(function ($) {
     // Vidio auto play
     // -------------------------------------------------------------
     (function () {
-    
+
     /* Vimeo API: http://developer.vimeo.com/player/js-api */
-    
+
         var iframe = document.getElementById('nofocusvideo');
         // $f == Froogaloop
         var player = $f(iframe);
@@ -261,7 +261,7 @@ jQuery(function ($) {
     $(window).load(function() {
 
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-         
+
         }else {
             $.stellar({
                 horizontalScrolling: false,
@@ -327,7 +327,7 @@ jQuery(function ($) {
     // -------------------------------------------------------------
 
     (function () {
-        var myLatlng = new google.maps.LatLng(41.372641, -74.687387);
+        var myLatlng = new google.maps.LatLng(20.6723694, -103.3416286);
 
             var styles = [
                 {
@@ -377,8 +377,7 @@ jQuery(function ($) {
                 title: 'Hello World!'
             });
 
-            var contentString = '' +
-                    '' +
+            var contentString = 'Aldama No. 135' +
                     '';
 
             var infowindow = new google.maps.InfoWindow({
@@ -393,8 +392,3 @@ jQuery(function ($) {
 
 
 });
-
-
-
-
-
